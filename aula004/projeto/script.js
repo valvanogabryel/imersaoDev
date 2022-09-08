0f32076b39cb3788520bbc4f38c153ca5a1ae3dc
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-08 10:25:59 -0300

    j

---
+++ aula004/projeto/script.js
@@ -0,0 +1,11 @@
+var listaFilmes = ['Harry Potter', 'Interstellar', 'Senhor dos Anéis']
+
+listaFilmes.push('Piratas do Caribe')
+listaFilmes.push('Percy Jackson')
+
+
+for (var indice = 0; indice < listaFilmes.length; indice++) {
+    console.log('<p>' + listaFilmes[indice] + '</p>')
+
+
+}
