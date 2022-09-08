984de2dd39796b757fe2cf7194843fa22de5b204
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-08 12:27:57 -0300

    j

---
+++ aula001/projeto/script.js
@@ -0,0 +1,27 @@
+function calcMedia() {
+    var nota1 = document.getElementById('notaum')
+    var nota2 = document.getElementById('notadois')
+    var nota3 = document.getElementById('notatres')
+    var nota4 = document.getElementById('notaquatro')
+    if (Number(nota1.value) < 0 || Number(nota1.value) > 10) {
+        alert('[ERRO] Insira um valor entre 0 e 10')
+    } else if (Number(nota2.value) < 0 || Number(nota2.value) > 10) {
+        alert('[ERRO] Insira um valor entre 0 e 10')
+    } else if (Number(nota3.value) < 0 || Number(nota3.value) > 10) {
+        alert('[ERRO] Insira um valor entre 0 e 10')
+    } else if (Number(nota4.value) < 0 || Number(nota4.value) > 10) {
+        alert('[ERRO] Insira um valor entre 0 e 10')
+    } else {
+        var notaFinal = (Number.parseInt(nota1.value) + Number.parseInt(nota2.value) + Number(nota3.value) + Number.parseInt(nota4.value)) / 4
+        var media = notaFinal.toFixed(1)
+        var resultado = document.getElementById('media')
+        resultado.innerHTML = `A sua nota média é ${media}`
+        resultado.style.paddingTop = '25px'
+
+        if (resultado > 7) {
+            alert('Aprovado')
+        } else {
+            alert('Reprovado')
+        }
+    }
+}
