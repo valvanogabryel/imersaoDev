b03b9dc26c470cd2dc2fd2952ee840c6a1395918
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-09 12:11:47 -0300

    j

--- aula002/projeto2/script.js
+++ aula002/projeto2/script.js
@@ -2,7 +2,7 @@ function converter() {
     var tempCelsius = document.getElementById('temp')
     var tempFaren = Number(tempCelsius.value) * 1.8 + 32
     var res = document.getElementById('tempConvertido')
-    res.innerHTML = `${tempFaren}°F`
+    res.innerHTML = `${tempFaren.toFixed(1)}°F`
 
     if (tempCelsius > 25 || tempFaren > 77) {
         document.body.style.backgroundImage = 'url(img/background-calor.jpg)'
