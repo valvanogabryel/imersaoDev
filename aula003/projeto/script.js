8f8c3a1c664a2951cb5c2365ad3dc5352420a248
valvanogabryel <109965223+valvanogabryel@users.noreply.github.com>
2022-09-12 11:28:49 -0300

    Update script.js

--- aula003/projeto/script.js
+++ aula003/projeto/script.js
@@ -2,6 +2,12 @@ var numSecreto = 9
 
 function chutar() {
     var chute = Number.parseInt(document.getElementById('valor').value)
+    var resposta = document.getElementById('res')
 
+    if (chute < numSecreto || chute > numSecreto) {
+        resposta.innerHTML = 'Errou'
+    } else {
+        resposta.innerHTML = 'Acertou'
+    }
 
 }
